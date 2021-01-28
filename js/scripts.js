fbd7ad1028bd13097afa2f78e2dba633762ce6a0
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-01-28 13:28:34 -0600

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,46 @@ var createElement = function (tagName, className, text) {
 
   return element;
 };
+
+
+
+var images = ['43273','44741','44766','44768','44783','44882','46172','46281'];
+var imgOutput = $_('.img-output');
+var counter = 0;
+
+var elControllerBtns = document.querySelectorAll('.btn');
+
+elControllerBtns.forEach(btn => {
+  btn.addEventListener('click', evt => {
+    if(evt.target.classList.contains('left-btn')) {
+      counter--
+      if(counter < 0) {
+        counter = images.length - 1
+      }
+      imgOutput.style.backgroundImage = `url(../img/${images[counter]}.jpg)`;
+    }
+    if(evt.target.classList.contains('right-btn')) {
+      counter++
+      if(counter >= images.length) {
+        counter = 0
+      }
+      imgOutput.style.backgroundImage = `url(../img/${images[counter]}.jpg)`;
+    }
+  });
+});
+
+function nextSlide () {
+  elControllerBtns.forEach(btn => {
+        if(counter < 0) {
+          counter = images.length - 1
+        imgOutput.style.backgroundImage = `url(../img/${images[counter]}.jpg)`;
+      }
+        counter++
+        if(counter >= images.length) {
+          counter = 0
+        }
+        imgOutput.style.backgroundImage = `url(../img/${images[counter]}.jpg)`;
+  });
+}
+
+setInterval(nextSlide, 2500);
